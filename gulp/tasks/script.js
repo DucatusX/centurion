cc7fb3331988751a39bf54520edb4e47dca52db0
Amary <laumiqv@gmail.com>
2021-02-04 14:33:33 +0300

    upd: add template, restructure

---
+++ gulp/tasks/script.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp')
+const webpack = require('webpack-stream')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin")
+const eslint = require('gulp-eslint')
+
+module.exports = function script() {
+  return gulp.src('src/js/main.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(webpack({
+      mode: 'production',
+      output: {
+        filename: '[name].min.js',
+      },
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      },
+      plugins: [
+        new CircularDependencyPlugin(),
+        new DuplicatePackageCheckerPlugin()
+      ]
+    }))
+    .pipe(gulp.dest('build/js'))
+}
+
---
+++ node_modules/editorconfig-cli/lib/actions/index.js
@@ -0,0 +1,7 @@
+var init = require('./init.js');
+var addRules = require('./add-rules.js');
+
+module.exports = {
+	init: init,
+	addRules: addRules
+};
